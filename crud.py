8ee6cd1e09ec7652359ad6f99e9993e698440df9
Khojialo <khojiwolverein@gmail.com>
2025-07-08 12:43:51 +0000

    update funksiya qo'shildi

--- crud.py
+++ crud.py
@@ -18,11 +18,11 @@ def insert_lesson_themes(lesson_name:str,themes:Optional[List[Theme]]=None):
         ss.commit()
 
 
-insert_lesson_themes("Python",[
-    Theme(title ="Database"),
-    Theme(title ="Sqlalchemy"),
-    Theme(title = "Github")
-])
+# insert_lesson_themes("Python",[
+#     Theme(title ="Database"),
+#     Theme(title ="Sqlalchemy"),
+#     Theme(title = "Github")
+# ])
 
 def show_all_lessons():
     with Session(engine) as ss:
@@ -31,6 +31,20 @@ def show_all_lessons():
         for lesson in lessons:
             print(lesson)
             for theme in lesson.themes:
-                print(f" Mavzu:{theme.title}")
+                print(f"Mavzu id:{theme.id} Mavzu nomi:{theme.title}")
 
 show_all_lessons()
+
+
+def update_info_themes(theme_id:int,new_theme_title:str) -> Theme:
+    with Session(engine) as ss:
+        stmt = select(Theme).where(Theme.id ==theme_id)
+        theme = ss.scalar(stmt)
+        theme.title = new_theme_title
+        ss.commit()
+        ss.refresh(theme)
+        return theme
+
+# update_info_themes(1,"Database.sql")
+
+
